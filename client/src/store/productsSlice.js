7f378a4d8a4a9993dcb98b9d5746b3e3536688f7
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-02-04 21:52:53 +0500

    redux toolkit setup

---
+++ client/src/store/productsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const productsSlice = createSlice({
+    name: 'products',
+    initialState: {
+        products: []
+    },
+    reducers: {
+
+    }
+})
+
+export const productAction = productsSlice.actions;
+export default productsSlice;
--- server/src/app.js
+++ server/src/app.js
@@ -8,4 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('short'));
 
+app.use(express.Router().get('/', (req, res) => {
+    res.status(200).send('hehe')
+}))
+
 export default app;
